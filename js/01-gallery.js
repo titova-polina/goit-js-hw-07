fa2ebfed1fb6e4a7a21f70f1d6b354c09c5f1243
titova-polina <112263937+titova-polina@users.noreply.github.com>
2023-06-18 23:08:35 +0300

    did tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const onClick = (event) => {
+  event.preventDefault();
+
+  const handler = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  };
+
+  document.addEventListener("keydown", handler);
+
+  const instance = basicLightbox.create(
+    `<img
+      class=""
+      src="${event.target.dataset.source}"
+      alt="${event.target.alt}"
+      />`,
+    {
+      onClose: () => document.removeEventListener("keydown", handler),
+    }
+  );
+
+  instance.show();
+};
+
+galleryList.addEventListener("click", onClick);
+
+const items = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg" target="_blank">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+      </li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", items);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const items = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", items);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
